1c09d299d8328e7f7c7d0f70dae9fd3f2ca4beb1
lchavarr <66494506+lchavarr@users.noreply.github.com>
2020-06-05 19:01:57 -0700

    Create update-version.ps1

---
+++ prime/update-version.ps1
@@ -0,0 +1,8 @@
+$version = $args[0]
+Write-Host "Setting version=[$version]."
+$FullPath = $args[1]
+Write-Host "File=[$FullPath]."
+
+[xml]$content = Get-Content $FullPath
+$content.package.metadata.version.InnerText = $version
+$content.Save($FullPath)
