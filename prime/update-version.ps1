81fcabc3a75d09a14984ecb33d3707f43071574c
lchavarr <66494506+lchavarr@users.noreply.github.com>
2020-06-05 19:50:35 -0700

    Delete update-version.ps1

--- prime/update-version.ps1
+++
@@ -1,9 +0,0 @@
-$version = $args[0]
-Write-Host "Setting version=[$version]."
-$FullPath = $args[1]
-Write-Host "File=[$FullPath]."
-
-[xml]$content = Get-Content $FullPath
-$content.package.metadata.version = $version
-echo $content.package.metadata
-$content.Save($FullPath)
